0ee9d5a9e4b4f5e4f7680b034431863b571c5759
Sebastian Tibaquirá <johnsebas94@gmail.com>
2021-08-17 11:42:33 -0500

    Create board code

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const{ dbConnection } = require ("./db/db");
 const Role  = require("./routes/role")
 const User = require("./routes/user")
 const Auth = require("./routes/auth");
+const Board = require("./routes/board");
 require("dotenv").config(); //Configurar las variables de entorno
 
 //Crear aplicacion
@@ -15,6 +16,7 @@ app.use(cors());
 app.use("/api/role", Role);
 app.use("/api/user", User);
 app.use("/api/auth", Auth);
+app.use("/api/board", Board);
 
 //Escuchar el puerto 3001
 app.listen(process.env.PORT, () => console.log("Backend server running OK on port: ", process.env.PORT));
---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  let jwtToken = req.header("Authorization");
+  if (!jwtToken) return res.status(400).send("Authorization denied: No token");
+  //[Bearer,3214mndsdighzkghej]
+  //[0 , 1]
+  console.log(jwtToken); //eliminar linea
+  jwtToken = jwtToken.split(" ")[1]; //dividir el bearen token por espacios y tomar el 1 del array
+  //3214mndsdighzkghej
+  if (!jwtToken) return res.status(400).send("Authorization denied: No token");
+  try {
+    const payload = await jwt.verify(jwtToken, process.env.SECRET_KEY_JWT);
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(400).send("Authorization denied: Invalid token");
+  }
+};
+
+module.exports = { auth };
